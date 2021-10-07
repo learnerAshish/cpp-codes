50491f84f1ab2417e1547dd2bd564981a5f46b10
learnerAshish <91174542+learnerAshish@users.noreply.github.com>
2021-10-07 21:47:42 +0530

    Add files via upload

---
+++ practical6/task5.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main()
+{
+     string a="";
+     string b="";
+     getline(cin,a);
+     getline(cin,b);
+     if(a.compare(b)==0)
+     {
+         cout<<"equal string "<<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;
+     }
+     else if(a.compare(b)>0)
+     {
+         cout<<"String a = "<< a <<"  is greater then b= "<< b <<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;
+     }
+      else
+     {
+         cout<<"string b = "<< b <<" is greater then a= "<< a <<endl;
+         cout<<"value returned by function compare is: "<<a.compare(b)<<endl;
+     }
+     return 0;
+}
